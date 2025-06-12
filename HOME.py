6fd8794b369520f1fffc91bb80f84a99c10756e4
MK <99416359+MK316@users.noreply.github.com>
2025-06-12 12:09:12 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.head("My Research Tools")
+st.markdown("#### My Research Tools")
 st.caption("This app showcases tools I’ve designed and developed to support my ongoing research. My work focuses on phonetics, phonology, digital literacy training, and English education related to speaking and listening. The tools are continuously updated and may be revised or expanded based on the direction of current and future research projects.")
